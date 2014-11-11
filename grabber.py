a183305f62e31ad0d64c19ed59a9aa5e572baa4c
EricSchles <ericschles@gmail.com>
2014-11-10 17:24:49 -0800

    testing

--- grabber.py
+++ grabber.py
@@ -2,3 +2,14 @@ import lxml.html
 import requests
 import grequests
 
+base_url = "http://newyork.craigslist.org/search/hhh"
+
+pages = []
+pages.append(base_url)
+for i in xrange(1,20):
+    pages.append(base_url+"?s="+str(i)+"00")
+    
+rs = (grequests.get(u) for u in pages)
+responses = grequests.map(rs)
+for r in responses:
+    
