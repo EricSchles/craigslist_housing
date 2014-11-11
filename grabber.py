4b37f877f6252483f8398aec102529b3ee75c633
EricSchles <ericschles@gmail.com>
2014-11-11 07:27:39 -0800

    working slightly better

--- grabber.py
+++ grabber.py
@@ -1,7 +1,7 @@
 import lxml.html
 import requests
 import grequests
-
+import pandas as pd
 base_url = "http://newyork.craigslist.org/search/hhh"
 
 pages = []
@@ -17,12 +17,26 @@ for r in responses:
     dicter = {}
     text = r.text.encode("ascii","ignore")
     html = lxml.html.fromstring(text)
-    dicter["time"] = html.xpath('//div[@class="content"]/span[@class="pl"]/time/@datetime')
-    dicter["title"] = html.xpath('//div[@class="content"]/span[@class="pl"]/a/')[0].text_content()
-    dicter["price"] = html.xpath('//div[@class="content"]/span[@class="l2"]/span[@class="price"]')[0].text_content()
-    dicter["housing"] = html.xpath('//div[@class="content"]/span[@class="l2"]/span[@class="housing"]')[0].text_content()
-    dicter["area"] = html.xpath('//div[@class="content"]/span[@class="l2"]/span[@class="pnr"]/small')[0].text_content()
-    
+    if html.xpath('//div[@class="content"]//span[@class="pl"]/time/@datetime') != []:
+        dicter["time"] = html.xpath('//div[@class="content"]//span[@class="pl"]/time')
+    else:
+        dicter["time"] = ''
+    if html.xpath('//div[@class="content"]//span[@class="pl"]/a') != []:
+        dicter["title"] = [x.text_content().encode("ascii","ignore") for x in html.xpath('//div[@class="content"]//span[@class="pl"]/a')]
+    else:
+        dicter["title"] = ''
+    if html.xpath('//div[@class="content"]/span[@class="l2"]//span[@class="price"]') != []:
+        dicter["price"] = [x.text_content().encode("ascii","ignore") for x in html.xpath('//div[@class="content"]//span[@class="l2"]/span[@class="price"]')]
+    else:
+        dicter["price"] = ''
+    if html.xpath('//div[@class="content"]//span[@class="l2"]/span[@class="housing"]') != []:
+        dicter["housing"] = [x.text_content().encode("ascii","ignore") for x in html.xpath('//div[@class="content"]//span[@class="l2"]/span[@class="housing"]')]
+    else:
+        dicter["housing"] = ''
+    if html.xpath('//div[@class="content"]//span[@class="l2"]/span[@class="pnr"]/small') != []:
+        dicter["area"] = [x.text_content().encode("ascii","ignore") for x in html.xpath('//div[@class="content"]//span[@class="l2"]/span[@class="pnr"]/small')]
+    else:
+        dicter["area"] = ''
     df = df.append(dicter,ignore_index=True)
 
 df.to_csv("results.csv")
