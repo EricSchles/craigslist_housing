2ac9c24e486573f71142e194cf9935e9880b9788
EricSchles <ericschles@gmail.com>
2014-11-11 06:50:46 -0800

    grabber finished

--- grabber.py
+++ grabber.py
@@ -12,8 +12,17 @@ for i in xrange(1,20):
 rs = (grequests.get(u) for u in pages)
 responses = grequests.map(rs)
 all_links = []
+df = pd.DataFrame()
 for r in responses:
+    dicter = {}
     text = r.text.encode("ascii","ignore")
     html = lxml.html.fromstring(text)
-    links = html.xpath('//div[@class="content"]/a/@href')
-    all_links.append(links)
+    dicter["time"] = html.xpath('//div[@class="content"]/span[@class="pl"]/time/@datetime')
+    dicter["title"] = html.xpath('//div[@class="content"]/span[@class="pl"]/a/')[0].text_content()
+    dicter["price"] = html.xpath('//div[@class="content"]/span[@class="l2"]/span[@class="price"]')[0].text_content()
+    dicter["housing"] = html.xpath('//div[@class="content"]/span[@class="l2"]/span[@class="housing"]')[0].text_content()
+    dicter["area"] = html.xpath('//div[@class="content"]/span[@class="l2"]/span[@class="pnr"]/small')[0].text_content()
+    
+    df = df.append(dicter,ignore_index=True)
+
+df.to_csv("results.csv")
